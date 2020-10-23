975e7dcc4559709b8e96d9ca41bf6c5f2eb76e35
DEV-SSG\golfmon <rusifer@naver.com>
2020-10-23 15:00:31 +0900

    외부 톰캣사용

---
+++ src/main/java/com/webtest/MySlippApplication.java
@@ -0,0 +1,16 @@
+package com.webtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+
+@SpringBootApplication
+@EnableJpaAuditing
+
+public class MySlippApplication {
+
+
+}
